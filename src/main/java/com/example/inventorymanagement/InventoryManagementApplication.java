8074ab5150174d3317dce0eeb0e8dc3886aa7345
Sandesh <30489233+j-sandy@users.noreply.github.com>
2026-01-08 10:14:15 +0530

    Add comment for Spring Boot application start
    Added a comment to indicate the start of the Spring Boot application.

--- src/main/java/com/example/inventorymanagement/InventoryManagementApplication.java
+++ src/main/java/com/example/inventorymanagement/InventoryManagementApplication.java
@@ -3,6 +3,7 @@ package com.example.inventorymanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Starting Spring Boot
 @SpringBootApplication
 public class InventoryManagementApplication {
 
